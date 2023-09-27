e55199643fc491cdf28c7dc1a86cf334caf0af4a
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2023-09-27 21:44:00 +0800

    edit role

--- HR/static/EditRole.js
+++ HR/static/EditRole.js
@@ -1,8 +1,22 @@
-// This is to handle for edit role funtion 
+$(document).ready(function() {
+    // Get the role ID from the URL or some other source
+    let roleID = ;
 
-$(document).ready(function(){
-    $("#submitButton").click(function(event){
-        console.log("Button clicked")
+    // Fetch the existing data for the role
+    axios.get(`http://0.0.0.0:5002/roles/${roleID}`)
+        .then(function(response) {
+            let role = response.data;
+            // Prepopulate the form with the existing data
+            $("#role_name").val(role.role_name);
+            $("#role_description").val(role.role_description);
+            $("#deadline").val(role.deadline);
+        })
+        .catch(function(error) {
+            console.error('Failed to fetch the role data:', error);
+        });
+
+    $("#submitButton").click(function(event) {
+        console.log("Button clicked");
         event.preventDefault();
         let roleData = {
             role_name: $("#role_name").val(),
@@ -10,14 +24,15 @@ $(document).ready(function(){
             deadline: $("#deadline").val()
         };
 
-        axios.post('http://0.0.0.0:5002/create_role', roleData)
-            .then(function (response) {
-                alert('Role created successfully!');
+        // Send a PUT request to update the role
+        axios.put(`http://0.0.0.0:5002/update_role/${roleID}`, roleData)
+            .then(function(response) {
+                alert('Role updated successfully!');
                 // Here, you can also close the modal or refresh the page
                 // $("#createRoleModal").modal('hide');
             })
-            .catch(function (error) {
-                alert('Failed to create the role. Please try again.');
+            .catch(function(error) {
+                alert('Failed to update the role. Please try again.');
             });
     });
 });
