41497aa1ed9176e952d576c40170ee33a82a1517
tiarahimawan <tiaramariah.2021@scis.smu.edu.sg>
2023-10-19 17:30:36 +0800

    UI for View Applicant Skills

--- frontend/src/views/ApplicantSkillView.vue
+++ frontend/src/views/ApplicantSkillView.vue
@@ -1,9 +1,93 @@
 <template>
+    <div class="View Applicant's Skills">
+    <div class="container text-center col-10 mt-5">
 
-  <div>
-    employee_id: 140002 <br>
+      <!-- ROLE DETAILS CARD GROUP -->
+      <div class="card-group mb-3">
+          <div class="card border-2 border-dark">
+            <div class="card-body">
+              <h6><strong>Role ID: #0100</strong></h6>
+            </div>
+          </div>
+          <div class="card border-2 border-dark">
+            <div class="card-body">
+              <h6><strong>Role Name: Software Engineer</strong></h6>
+            </div>
+          </div>
+          <div class="card border-2 border-dark">
+            <div class="card-body">
+              <h6><strong>Department: Engineering</strong></h6>
+            </div>
+          </div>
+      </div>
 
-    employee_skills: {{ employee_skills }}
+
+      <div class="row mb-3">
+        <!-- APPLICANT'S DETAILS -->
+        <div class="col-9">
+        <div class="card text-start">
+          <div class="card-header text-bg-dark">
+            <h5><strong>Applicant's Personal Details</strong></h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              <h6><strong>ID: 150065</strong></h6>
+            </li>
+            <li class="list-group-item">
+              <h6><strong>Name: Noah Goh</strong></h6>
+            </li>
+            <li class="list-group-item">
+              <h6><strong>Email: Noah.Goh@allinone.com.sg</strong></h6>
+            </li>
+            <li class="list-group-item">
+              <h6><strong>Department: Engineering</strong></h6>
+            </li>
+            <li class="list-group-item">
+              <h6><strong>Country: Singapore</strong></h6>
+            </li>
+          </ul>
+        </div> 
+        </div>
+
+        <!-- PERCENTAGE SKILL MATCH -->
+        <div class="col-3">
+        <div class="card" style="height:100%">
+          <div class="card-header text-bg-dark">
+            <h5><strong>Percentage Skill Match</strong></h5>
+          </div>
+          <div class="card-body d-flex align-items-center justify-content-center">
+            <h1><strong>50%</strong></h1>
+          </div>
+        </div> 
+        </div>
+      </div>
+
+      <!-- APPLICANT'S SKILLS VS REQUIRED SKILLS TABLE -->
+      <div class="row">
+        <table class="table table-striped">
+          <thead class="table-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Skill</th>
+              <th scope="col">Possessed by Applicant</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Node.js</td>
+              <td>Yes</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Javascript</td>
+              <td>No</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
   </div>
 </template>
 
