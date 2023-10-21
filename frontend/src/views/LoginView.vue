7dd98e34e9a0cfcd66d0a2875847775f42dcaae4
Davina Wijaya <phandavina@gmail.com>
2023-10-22 01:50:55 +0800

    added rerouting based on user type

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -18,16 +18,40 @@ export default {
       userID: ''
     };
   },
+  computed: {
+    logged_in_staff() {
+      return this.$store.logged_in_staff
+    }
+  },
   methods: {
-    handleLogin() {
-      if (this.userID) {
-        this.$store.dispatch('login', this.userID);
-        console.log(this.$store.logged_in_staff)
-      } else {
-        alert('Please enter a User ID');
+  async handleLogin() {
+    if (this.userID) {
+      try {
+        await this.$store.dispatch('login', this.userID);
+        const role = this.$store.state.logged_in_staff?.role;
+
+        if (role) {
+          switch(role) {
+            case "User":
+              this.$router.push('/apply-role');
+              break;
+            case "HR":
+              this.$router.push('/posting');
+              break;
+            default:
+              this.$router.push('/posting');
+          }
+        }
+      } catch (error) {
+        console.error('Login failed:', error);
+        alert('Login failed, please try again.');
       }
+    } else {
+      alert('Please enter a User ID');
     }
   }
+}
+
 }
 </script>
 
